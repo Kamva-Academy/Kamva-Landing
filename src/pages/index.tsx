199132509f742a45d53053fac5e01cbd7f75c78e
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-07 14:14:01 +0330

    fix base url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 
 
-import Headline from "@/components/Headline/Headline";
-import Benefits from "@/components/benefits/Benefits";
-import Comments from "@/components/comment/Comment";
-import Courses from "@/components/courses/Courses";
-import Pereview from "@/components/pereview/Pereview";
-import Footer from "@/layout/Footer";
-import Header from "@/layout/Header";
+import Headline from "components/Headline/Headline";
+import Benefits from "components/benefits/Benefits";
+import Comments from "components/comment/Comment";
+import Courses from "components/courses/Courses";
+import Pereview from "components/pereview/Pereview";
+import Footer from "layout/Footer";
+import Header from "layout/Header";
 import { Fragment } from "react";
 
 
