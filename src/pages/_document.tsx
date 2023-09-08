666e64bdecb23db42b03e78670b56e3b2cca2e7f
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-08 10:46:12 +0330

    remove redundant fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,10 @@ export default function Document() {
   return (
     <Html lang="fa" dir='rtl'>
       <Head>
-        <link />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap" rel="stylesheet" />
         <link href="https://db.onlinewebfonts.com/c/235b71a9b409e684e865eb4a996e925e?family=IRANYekan" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" ></link>
         <link href="https://fonts.googleapis.com/css2?family=Lalezar&family=Nanum+Gothic&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
