52da1682c642a2dcbe38956faed10a9089b9c522
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-23 01:51:49 +0330

    remove redundant logs

--- src/components/organisms/Comment/slider/slider.js
+++ src/components/organisms/Comment/slider/slider.js
@@ -1,9 +1,7 @@
 
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation} from 'swiper/modules';
-
-// Import Swiper styles
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -19,44 +17,38 @@ SwiperCore.use([Autoplay]);
 export default () => {
 
   return (
-      <Swiper 
-      style={{width:"80%" , overflowY:"visible"}}
+    <Swiper
+      style={{ width: "80%", overflowY: "visible" }}
       spaceBetween={50}
       slidesPerView={1}
       breakpoints={
         {
-          2000:{
-            slidesPerView:4
+          2000: {
+            slidesPerView: 4
           },
-          1500:{
-            slidesPerView:3
+          1500: {
+            slidesPerView: 3
           },
           1000:
           {
-            slidesPerView:2
+            slidesPerView: 2
           }
-          
+
         }
-      }
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-      <SliderCard />
+      }>
+      <SwiperSlide style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <SliderCard />
       </SwiperSlide>
-      <SwiperSlide style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-      <SliderCard />
+      <SwiperSlide style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <SliderCard />
       </SwiperSlide>
-      <SwiperSlide style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-      <SliderCard />
+      <SwiperSlide style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <SliderCard />
       </SwiperSlide>
-      <SwiperSlide style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-      <SliderCard />
+      <SwiperSlide style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <SliderCard />
       </SwiperSlide>
 
     </Swiper>
-    
- 
- 
   );
 };
