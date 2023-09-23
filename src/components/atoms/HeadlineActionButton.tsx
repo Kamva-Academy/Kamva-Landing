def8f8c7db893c31e66ae698b1a20b8313f5e1b7
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-09-24 02:47:22 +0330

    refactor HeadlineActionButton

--- src/components/atoms/HeadlineActionButton.tsx
+++ src/components/atoms/HeadlineActionButton.tsx
@@ -11,7 +11,15 @@ const HeadlineActionButton = ({ title }: HeadlineActionButtonPropsType) => {
   }
 
   return (
-    <Button onClick={scrollIntoView} sx={{ width: { xs: 160, sm: 200, md: 240, lg: 280 }, height: { xs: 60, sm: 70, md: 80, lg: 90 }, border: "1px solid #3498DB", background: "rgba(255, 211, 88, 1) !important", borderRadius: 3, boxShadow: "2px 2px 4px 0px rgba(0, 0, 0, 0.25)" }}>
+    <Button onClick={scrollIntoView}
+      sx={{
+        width: { xs: 160, sm: 200, md: 240, lg: 280 },
+        height: { xs: 60, sm: 70, md: 80, lg: 90 },
+        border: "1px solid #3498DB",
+        background: "rgba(255, 211, 88, 1) !important",
+        borderRadius: 6,
+        boxShadow: "2px 1px 4px 0px rgba(0, 0, 0, 0.25)"
+      }}>
       <Typography color={'black'} fontSize={{ xs: 16, sm: 20, md: 24, lg: 28 }}>
         {title}
       </Typography>
