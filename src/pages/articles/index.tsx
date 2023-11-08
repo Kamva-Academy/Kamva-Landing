3b0cabfdfe6fc7578d69cd8600a4109e08ec5e98
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-11-08 10:47:13 +0330

    complere articles page

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,24 +1,44 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDataAction } from '../../store/action';
 import { fetchData } from '../../components/apiService/doc';
+import { useEffect, useState } from 'react';
+import ArticleCard from 'components/organisms/cards/articleCard';
+import { Grid } from '@mui/material';
 
 const MyComponent: React.FC = () => {
   const dispatch = useDispatch();
   const data = useSelector((state: any) => state.data.data);
-
-  const fetchAndSaveData = async () => {
-    try {
-      const result = await fetchData();
-      dispatch(fetchDataAction(result));
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
+  const [art , setArt ] = useState([])
+  useEffect(()=>{
+    const fetchAndSaveData = async () => {
+      try {
+        const result = await fetchData();
+        dispatch(fetchDataAction(result));
+        setArt(result)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchAndSaveData()
+  },[])
   return (
     <div>
-      <button onClick={fetchAndSaveData}>Fetch Data</button>
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      <Grid container>
+
+      {
+       art.map(e=>{
+        console.log(e)
+        return(
+          <Grid md={4}>
+               <ArticleCard id={e.id} name={e.name} description={e.description}  cover_page={e.cover_page} />
+          </Grid>
+        )
+          }
+        )
+       }
+  
+      </Grid>
+    
     </div>
   );
 };
