bd255c43ceb04fcd58a0064ae157064a45fd90f6
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-11-10 14:59:02 +0330

    fix TopSiteBanner images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,8 @@ function Home() {
   return (
     <Fragment>
       <TopSiteBanner banner={{
-        mobile_image: 'https://kamva-minio-storage.darkube.app/media/websiteappearance/top-header-saraamad.jpg',
-        desktop_image: 'https://kamva-minio-storage.darkube.app/media/websiteappearance/top_header_-_mobile_-_saraamad.jpg',
+        mobile_image: 'https://kamva-minio-storage.darkube.app/media/websiteappearance/top_header_-_mobile_-_saraamad.jpg',
+        desktop_image: 'https://kamva-minio-storage.darkube.app/media/websiteappearance/top-header-saraamad.jpg',
         redirect_to: 'https://platform.kamva.academy/login/?utm_source=landing-banner&utm_medium=organic&utm_campaign=saraamad',
       }} />
       <AppBar />
