03cf8f86f765b8397a01dd8e2bc52c29364bb1f2
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-11-10 13:46:23 +0330

    fix article page with TOF

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,17 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchDataAction } from '../../store/action';
-import { fetchData } from '../../components/apiService/doc';
+import { fetchDataAction } from 'store/action';
+import { fetchData } from 'components/apiService/doc';
 import { Fragment, useEffect, useState } from 'react';
 import ArticleCard from 'components/organisms/cards/articleCard';
 import { Grid, Typography } from '@mui/material';
-import TopSiteBanner from 'components/molecules/TopSiteBanner';
 import AppBar from 'components/organisms/AppBar';
 
 const MyComponent: React.FC = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state: any) => state.data.data);
-  const [art , setArt ] = useState([])
-  useEffect(()=>{
+  const [art, setArt] = useState<any[]>([])
+
+  useEffect(() => {
     const fetchAndSaveData = async () => {
       try {
         const result = await fetchData();
@@ -22,34 +21,23 @@ const MyComponent: React.FC = () => {
       }
     };
     fetchAndSaveData()
-  },[])
+  }, [])
+
   return (
     <Fragment>
       <AppBar />
       <div className='flex flex-col justify-center w-full items-center'>
-        <Typography sx={{fontSize:{md:"36px" , xs:"30px"} , fontWeight:"900" , fontFamily: "iranyekan", paddingBlock:"4rem"}}>
+        <Typography sx={{ fontSize: { md: 36, xs: 30 }, fontWeight: 900, paddingBlock: "4rem" }}>
           مقاله‌ها
         </Typography>
-      <div className='  w-11/12 '>
-      <Grid container >
-
-{
- art.map(e=>{
-  console.log(e)
-  return(
-    <Grid md={4} padding={"10px"} lg={3} sm={6} xs={12} >
-         <ArticleCard id={e.id} name={e.name} description={e.description}  cover_page={e.cover_page} />
-    </Grid>
-  )
-    }
-  )
- }
-
-</Grid>
+        <Grid container >
+          {art.map(e =>
+            <Grid md={4} padding={10} lg={3} sm={6} xs={12} >
+              <ArticleCard id={e.id} name={e.name} description={e.description} cover_page={e.cover_page} />
+            </Grid>
+          )}
+        </Grid>
       </div>
-      </div>
-    
-    
     </Fragment>
   );
 };
