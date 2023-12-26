f9036bbefd874322934b72adca1b3151a3f4ce63
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2023-12-26 15:16:00 +0330

    add skeleton in programs cart

--- src/pages/programs/index.tsx
+++ src/pages/programs/index.tsx
@@ -3,6 +3,7 @@ import { Grid, Typography, Stack, Container } from "@mui/material";
 import AppBar from "components/organisms/AppBar";
 import ProgramCard from "components/organisms/cards/ProgramCard";
 import { useGetProgramsQuery } from "redux/features/programSlice";
+import EventSkeletonCard from "components/organisms/cards/SkeletonCard";
 // import EventSkeletonCard from "components/organisms/cards/EventSkeletonCard";
 
 const Programs: React.FC = () => {
@@ -16,33 +17,63 @@ const Programs: React.FC = () => {
     error,
     refetch
   } = useGetProgramsQuery(1);
-
-  return (
-    <Fragment>
-      <AppBar />
-      <Container maxWidth='lg'>
-        <Stack width='100%'>
-          <Typography
-            sx={{
-              fontSize: { md: "36px", xs: "30px" },
-              fontWeight: "900",
-              fontFamily: "iranyekan",
-              paddingBlock: "4rem",
-              textAlign: "center",
-            }}>
-            {'دوره‌ها'}
-          </Typography>
-          <Grid container spacing={2}>
-            {programs.map((program) =>
-              <Grid item xs={12} sm={6} md={4} >
-                <ProgramCard event={program} />
-              </Grid>
-            )}
-          </Grid>
-        </Stack>
-      </Container>
-    </Fragment>
-  );
+  if(isLoading == true){
+    return (
+      <Fragment>
+        <AppBar />
+        <Container maxWidth='lg'>
+          <Stack width='100%'>
+            <Typography
+              sx={{
+                fontSize: { md: "36px", xs: "30px" },
+                fontWeight: "900",
+                fontFamily: "iranyekan",
+                paddingBlock: "4rem",
+                textAlign: "center",
+              }}>
+              {'دوره‌ها'}
+            </Typography>
+            <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={4}><EventSkeletonCard /></Grid>
+                   <Grid item xs={12} sm={6} md={4}>  <EventSkeletonCard /></Grid>
+                   <Grid item xs={12} sm={6} md={4}>  <EventSkeletonCard /></Grid>
+                   <Grid item xs={12} sm={6} md={4}>  <EventSkeletonCard /></Grid>
+                   <Grid item xs={12} sm={6} md={4}>  <EventSkeletonCard /></Grid>
+                   <Grid item xs={12} sm={6} md={4}>  <EventSkeletonCard /></Grid>
+            </Grid>
+          </Stack>
+        </Container>
+      </Fragment>
+    );
+  }
+  else{
+    return (
+      <Fragment>
+        <AppBar />
+        <Container maxWidth='lg'>
+          <Stack width='100%'>
+            <Typography
+              sx={{
+                fontSize: { md: "36px", xs: "30px" },
+                fontWeight: "900",
+                fontFamily: "iranyekan",
+                paddingBlock: "4rem",
+                textAlign: "center",
+              }}>
+              {'دوره‌ها'}
+            </Typography>
+            <Grid container spacing={2}>
+              {programs.map((program) =>
+                <Grid item xs={12} sm={6} md={4} >
+                  <ProgramCard event={program} />
+                </Grid>
+              )}
+            </Grid>
+          </Stack>
+        </Container>
+      </Fragment>
+    );
+  }
 };
 
 export default Programs;
