1c70ad9cf2a775e4169a7f4c666aebcc4de17072
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-03-18 04:26:39 +0330

    get just Kamva events

--- src/redux/features/programSlice.tsx
+++ src/redux/features/programSlice.tsx
@@ -4,7 +4,7 @@ import { ProgramType } from 'components/types/models';
 export const extendedApiSlice = api.injectEndpoints({
   endpoints: builder => ({
     getPrograms: builder.query<any, { pageNumber: number }>({
-      query: ({ pageNumber }) => `fsm/event/?page=${pageNumber}&is_private=False`,
+      query: ({ pageNumber }) => `fsm/event/?page=${pageNumber}&is_private=False&party=595cf997-87ed-4914-8791-e551a9497776`,
       transformResponse: (response: any): ProgramType[] => {
         return response.results;
       },
